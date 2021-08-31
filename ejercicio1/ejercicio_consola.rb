4a580d2084b8db4c69f19a5d5e95ba9001180bcb
Byskachita <denisserosseln@gmail.com>
2021-08-30 23:02:16 -0400

    agregando un espacio de prueba

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,18 @@
+puts "¡Hola Mundo!"
+puts ("¿cómo estas?")
+
+puts "Ingresa tu nombre"
+nombre_usuario = gets.chomp
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿que edad tienes?"
+edad = gets.chomp
+puts "#{edad} eso es mucho tiempo "
+
+puts "¿donde vives?"
+lugar_vivienda = gets.chomp
+puts "¿dónde queda #{lugar_vivienda}? ??"
+
+
